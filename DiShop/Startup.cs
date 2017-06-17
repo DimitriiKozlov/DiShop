9a55ac74c6e3223f494f9df0ab4488c52a2bb4ff
Dimitrii <ikzdimitrij@gmail.com>
2017-06-17 18:59:07 +0300

    Add project files.

---
+++ DiShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiShop.Startup))]
+namespace DiShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
