d88286d2e754f974a448ec7f5c76c6efa6de9d16
Dimitrii <ikzdimitrij@gmail.com>
2017-06-23 04:09:44 +0300

    Improve Vehicle view page

--- DiShop/Models/DiShopDbInitializer.cs
+++ DiShop/Models/DiShopDbInitializer.cs
@@ -24,7 +24,7 @@ namespace DiShop.Models
                 new Vehicle{Id=new Guid("86938F23-2954-E711-A9D7-3085A911C1C7"),Name= "M2 Bradley",YearOfDesigned= 1981, Cost = 1840000, Weight = 27600, Length = 6550, Width = 3600, Height = 2980, Crew = 3, ArmorId = new Guid("159370F0-2554-E711-A9D7-3085A911C1C7"), BuyCount = 0},
                 new Vehicle{Id=new Guid("5997EFF8-2954-E711-A9D7-3085A911C1C7"),Name= "IAV Stryker",YearOfDesigned= 2002, Cost = 5110000, Weight = 18770, Length = 6950, Width = 2720, Height = 2640, Crew = 2, ArmorId = new Guid("159370F0-2554-E711-A9D7-3085A911C1C7"), BuyCount = 0},
                 new Vehicle{Id=new Guid("96C43B65-2A54-E711-A9D7-3085A911C1C7"),Name= "LAV-25",YearOfDesigned= 1983, Cost = 2420000, Weight = 12800, Length = 6390, Width = 2500, Height = 2690, Crew = 3, ArmorId = new Guid("159370F0-2554-E711-A9D7-3085A911C1C7"), BuyCount = 0},
-                new Vehicle{Id=new Guid("8F7C1DBE-2A54-E711-A9D7-3085A911C1C7"),Name= "AAVP7A1 RAM/RS",YearOfDesigned= 1972, Cost = 1950000, Weight = 29100, Length = 7940, Width = 3270, Height = 3260, Crew = 3, ArmorId = new Guid("5AD743F7-2554-E711-A9D7-3085A911C1C7"), BuyCount = 0}
+                new Vehicle{Id=new Guid("8F7C1DBE-2A54-E711-A9D7-3085A911C1C7"),Name= "AAVP7A1 RAM RS",YearOfDesigned= 1972, Cost = 1950000, Weight = 29100, Length = 7940, Width = 3270, Height = 3260, Crew = 3, ArmorId = new Guid("5AD743F7-2554-E711-A9D7-3085A911C1C7"), BuyCount = 0}
             };
             vehicles.ForEach(s => context.Vehicles.Add(s));
             context.SaveChanges();
